32184a73c6d334dc38f12582c8a1aad997f02209
Adrian Suciu <adrian.suciu@analog.com>
2021-11-10 10:18:20 +0200

    Added git_config

---
+++ git_config.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+git config --global user.name "Adrian Suciu"
+git config --global user.email "adrian.suciu@analog.com"
+git config --global credential.helper store
+git config --global core.editor vim
+
